966b484a999d48c94bbd38367b7beb1f74d0f778
nura tabanjeh <nuratabanjeh96@gmail.com>
2021-04-25 15:23:10 +0300

    trying

---
+++ js/joinus.js
@@ -0,0 +1,42 @@
+'use strict';
+
+// eslint-disable-next-line no-unused-vars
+function Inputs(name, email, service, phone, experience, location, discription, photo) {
+    this.name = name;
+    this.email = email;
+    this.service = service;
+    this.phone = phone;
+    this.experience = experience;
+    this.location = location;
+    this.discription = discription;
+    this.photo = photo;
+
+
+}
+
+let joinBtn = document.getElementById('joinBtn');
+joinBtn.addEventListener('click', join());
+
+function join() {
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let service = document.getElementById('service').value;
+    let phone = document.getElementById('phone').value;
+    let experience = document.getElementById('experience').value;
+    let location = document.getElementById('location').value;
+    let discription = document.getElementById('discription').value;
+    let photo = document.getElementById('photo').value;
+    let data = new Inputs(document.getElementById('name').value, email, service, phone, experience, location, discription, photo);
+    saveJoinData(data);
+}
+
+
+
+
+
+
+function saveJoinData(inputs) {
+    let data = JSON.stringify(inputs);
+    localStorage.setItem('worker inputs', data);
+
+}
