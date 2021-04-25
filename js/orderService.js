87a8c9e160abcd608ed407ce0190a4c23ca0feea
YousefAbu-Jalboush <yousef.y.jalboush@gmail.com>
2021-04-26 02:25:13 +0300

    Add Card + Edit Css

--- js/orderService.js
+++ js/orderService.js
@@ -60,3 +60,92 @@ function renderOrder (){
   p2.textContent = `We will contact you as soon as possible on the following phone number ${OrderService.arrayOfOrder[index].phoneNumber} .`;
 
 }
+
+
+// ************************************
+
+let workerArr = [] ;
+function getJoinData(){
+  let data = localStorage.getItem('workerInputs');
+
+  let workerInputs = JSON.parse(data);
+
+  if(workerInputs !== null){
+    workerArr = workerInputs;
+  }
+  // console.log(workerArr);
+  renderCardOnOrederService();
+}
+
+getJoinData();
+
+function renderCardOnOrederService() {
+
+  // console.log(workerArr[0].photo);
+  let serviceSelect = document.getElementById('service');
+
+  let containerCard = document.getElementById('containerCard');
+
+  let divBox ;
+
+  let divImgBx;
+  let img ;
+
+  let divContent;
+  let h3 ;
+  let pName ;
+  let pExperience ;
+  let pLocation ;
+  let pDiscription ;
+
+  let serviceOption ;
+
+  for (let i = 0; i < workerArr.length ; i++) {
+
+    serviceOption = document.createElement('option');
+    serviceSelect.appendChild(serviceOption);
+    serviceOption.textContent = `${workerArr[i].service} - ${workerArr[i].name}`;
+    serviceOption.setAttribute('value',`${workerArr[i].service} - ${workerArr[i].name}`);
+
+    divBox = document.createElement('div');
+
+    divImgBx = document.createElement('div');
+    img = document.createElement('img');
+
+    divContent = document.createElement('div');
+    h3 = document.createElement('h3');
+    pName = document.createElement('p');
+    pExperience = document.createElement('p');
+    pLocation = document.createElement('p');
+    pDiscription = document.createElement('p');
+
+    containerCard.appendChild(divBox);
+    divBox.setAttribute('class','box mix other');
+
+    divBox.appendChild(divImgBx);
+    divImgBx.setAttribute('class','imgBx');
+    divImgBx.appendChild(img);
+    console.log(workerArr[i].photo);
+    if( workerArr[i].photo === '' || workerArr[i].photo === null ){
+      img.setAttribute('src','../imgYousef/default.png');
+    }else{
+      img.setAttribute('src', workerArr[i].photo);
+      img.setAttribute('alt', 'The entered image link is invalid');
+    }
+
+    divBox.appendChild(divContent);
+    divContent.setAttribute('class','content');
+    divContent.appendChild(h3);
+    divContent.appendChild(pName);
+    divContent.appendChild(pExperience);
+    divContent.appendChild(pLocation);
+    divContent.appendChild(pDiscription);
+
+    h3.textContent = workerArr[i].service;
+    pName.textContent = `Name : ${workerArr[i].name}`;
+    pExperience.textContent = `Experience : ${workerArr[i].experience}`;
+    pLocation.textContent = `Location : ${workerArr[i].location}`;
+    pDiscription.textContent = `Discription : ${workerArr[i].discription}`;
+
+  }
+}
