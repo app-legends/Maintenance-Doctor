9a3671cba0af22f1068a7b65af5bd88194320272
nura tabanjeh <nuratabanjeh96@gmail.com>
2021-04-26 21:59:49 +0300

    finish index page

--- js/SlideShow.js
+++ js/SlideShow.js
@@ -1,45 +1,23 @@
 /* eslint-disable no-unused-vars */
 'use strict';
-let slideIndex = 1;
-showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+var slideIndex = 0;
+showSlides();
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName('mySlides');
-  let dots = document.getElementsByClassName('dot');
-  if (n > slides.length) {slideIndex = 1;}
-  if (n < 1) {slideIndex = slides.length;}
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none';
+    slides[i].style.display = "none";
   }
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1 }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(' active', '');
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = 'block';
-  dots[slideIndex-1].className += ' active';
+  slides[slideIndex - 1].style.display = "block";
+
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
 }
 
-// let slideIndex = 0;
-// showSlides();
-
-// function showSlides() {
-//   let i;
-//   let slides = document.getElementsByClassName('mySlides');
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = 'none';
-//   }
-//   slideIndex++;
-//   if (slideIndex > slides.length) {slideIndex = 1;}
-//   slides[slideIndex-1].style.display = 'block';
-//   setTimeout(showSlides, 2000); // Change image every 2 seconds
-// }
