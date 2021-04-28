850726a55daddf0391102fe7e6c510ddea731a9c
YousefAbu-Jalboush <yousef.y.jalboush@gmail.com>
2021-04-28 12:10:01 +0300

    test

--- js/nightMode.js
+++ js/nightMode.js
@@ -8,9 +8,8 @@ let lightMode;
 let darkMode;
 
 if (window.location.pathname === '/index.html') {
-  console.log(window.location.pathname);
-  lightMode = '../../css/colors.css';
-  darkMode = '../../css/nightColors.css';
+  lightMode = './css/colors.css';
+  darkMode = './css/nightColors.css';
 } else {
   lightMode = '../css/colors.css';
   darkMode = '../css/nightColors.css';
@@ -22,12 +21,10 @@ if (b !== null) nightMode();
 // eslint-disable-next-line no-unused-vars
 function nightMode() {
   if (checkBox.checked === true || b === 'true') {
-    // link.setAttribute('href', `${darkMode}`);
     link.setAttribute('href', darkMode);
     checkBox.checked = true;
     b = true;
   } else {
-    // link.setAttribute('href', `${lightMode}`);
     link.setAttribute('href', lightMode);
     b = false;
     checkBox.checked = false;
